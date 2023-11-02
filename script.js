e5cacf9bd424472133bb47b793b1dfa8da82336e
Diana <otievidiana@gmail.com>
2023-11-02 12:51:23 +0400

    changes

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
+const result=document.getElementById('result');
 let newOperation = false;
+
 function display(value){
     if (newOperation) {
-        document.getElementById("result").value = '';
+        result.value = '';
         newOperation = false;
     }
-    document.getElementById("result").value += value;
+   result.value += value;
 }
 
 function calculate(){
-    let x = document.getElementById("result").value;
+    let x = result.value;
     try {
         let y = eval(x);
-        document.getElementById("result").value = y;
+        result.value = y;
         newOperation = true;  // Set the flag after a calculation
     } catch (e) {
-        document.getElementById("result").value = 'Error';
+        result.value = 'Error';
     }
 }
 
-function clearInput(){
-    document.getElementById('result').value = ''
-}
+
+document.getElementById('clear').addEventListener('click', function() {
+    result.value = '';
+});
 
 
+document.addEventListener('keydown', function(event) {
+    const validKeys = "0123456789/*-+.";
+    let x = result.value;
+    if (validKeys.includes(event.key) || event.key === 'Enter' || event.key === 'Backspace') {
+        event.preventDefault();  // Prevent default action (e.g., scrolling on pressing space)
+        if (event.key === 'Enter') {
+            try {
+             let y = eval(x);  
+               result.value=y;
+                newOperation=true;
+            } catch {
+                result.value = 'Error';
+            }
+        } else if (event.key === 'Backspace') {
+            result.value = result.value.slice(0, -1);
+        } else if(newOperation){
+            result.value = '';
+            newOperation = false;
+            
+        }else{
+            result.value += event.key;
+        }
+    }
+});
 
